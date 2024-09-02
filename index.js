9c6a418912583c6d93d06b31295f7520bdf2fb7b
estvl <esat.shehaj@gmail.com>
2024-09-02 13:32:43 +0200

    Updated index.js with [brief description of the changes]

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World! CI/CD Pipeline is working.');
+    res.send('Hello World! Si jeni?.');
 });
 
 const PORT = process.env.PORT || 3000;
